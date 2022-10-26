54d759ee3028b7e7f5c61f0b26070db35c7935f7
Timur <timurram007@gmail.com>
2022-10-26 16:37:45 +0300

    done register api

--- src/components/Pages/Register/Register.jsx
+++ src/components/Pages/Register/Register.jsx
@@ -3,8 +3,13 @@ import { useEffect, useState, useRef } from "react";
 import s from './Register.module.css';
 import { NavLink } from "react-router-dom";
 import { CInput, useInput } from "../../UI/CInput/CInput";
+import axios from "../../../api/axios";
+
+const REGISTER_URL = '/create_user'
 
 const Register = () => {
+    const [error, setError] = useState('')
+    const [success, setSuccess] = useState(false)
     const name = useInput('', { isEmpty: true })
     const surname = useInput('', { isEmpty: true })
     const login = useInput('', { isEmpty: true })
@@ -14,26 +19,55 @@ const Register = () => {
 
     const formValid = [name.allValid, surname.allValid, login.allValid, email.allValid, pwd.allValid, repeatPwd.allValid].every(item => item)
 
-    const register = (e) => {
+    const register = async (e) => {
         e.preventDefault()
-        console.log(name, surname, email, login, pwd, repeatPwd)
+        try {
+            const response = await axios.post(
+                REGISTER_URL,
+                JSON.stringify({
+                    name: name.value,
+                    surname: surname.value,
+                    login: login.value,
+                    email: email.value,
+                    pwd: pwd.value
+                }),
+                {
+                    headers: {
+                        'accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    }
+                }
+            )
+            setError('')
+            setSuccess(true)
+        } catch (err) {
+            if (!err?.response) {
+                setSuccess(false)
+                setError('Сервер не отвечает')
+            } else {
+                setSuccess(false)
+                setError(err?.response?.data?.message)
+            }
+        }
     }
 
     return (
         <div className={s.form_wrapper}>
             <form className={s.register_form} onSubmit={register}>
                 <h1>Регистрация</h1>
+                {error ? <p data-banner-type='error'>{error}</p> : ''}
+                {success ? <p data-banner-type='success'>Аккаунт создан</p> : ''}
                 <CInput title='Имя' autoRef={true} type='text' instance={name} />
                 <CInput title='Фамилия' type='text' instance={surname} />
                 <CInput title='Почта' type='text' instance={email} />
                 <CInput title='Имя пользователя' type='text' instance={login} />
                 <CInput title='Пароль' type='password' instance={pwd} />
                 <CInput title='Повторите пароль' type='password' instance={repeatPwd} />
-                <button disabled={!formValid}>Зарегестрироваться</button>
+                <button disabled={!formValid || repeatPwd.value === ''}>Зарегестрироваться</button>
                 <p style={{ marginBottom: 0, marginTop: "10px" }}>Уже есть аккаунт?</p>
                 <NavLink to="/login">Войти</NavLink>
             </form>
-        </div>
+        </div >
     )
 }
 
