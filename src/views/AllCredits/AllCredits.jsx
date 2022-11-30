d8c059ce1b87abd6685266f14ad4f8d61c02ecf6
Timur <timurram007@gmail.com>
2022-11-30 19:10:49 +0300

    template all credits

--- src/views/AllCredits/AllCredits.jsx
+++ src/views/AllCredits/AllCredits.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
+import useAxios from '../../hooks/useAxios'
 import s from './AllCredits.module.css'
 
 const AllCredits = () => {
     const { t } = useTranslation()
+    const api = useAxios()
+
+    useEffect(() => {
+        api.get('api/my_credits').then(res => {
+            console.log(res)
+        }).catch(err => {
+            console.log(err)
+        })
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
     return (
         <div className={s.container}>
